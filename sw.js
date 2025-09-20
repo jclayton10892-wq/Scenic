f25327464c70c5e2170ac941b897ec33ac1a5f00
jclayton10892-wq <jclayton10892@gmail.com>
2025-09-20 15:36:10 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='srq-master-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png','./quote-mobile.css','./quote-mobile.js'])));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
